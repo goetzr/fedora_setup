2602a353f9679cb030ae7d8695ea846e6b2f5168
Russ Goetz <russgoetz@gmail.com>
2025-08-16 17:06:02 -0400

    Now installing xclip when running X11.

--- install/development.sh
+++ install/development.sh
@@ -9,6 +9,13 @@ rm -rfd ~/.config/nvim &> /dev/null || true
 mkdir ~/.config/nvim
 stow -d ~/dotfiles nvim
 
+# Install xclip to use as the clipboard provider for neovim when running X11.
+if [[ -n $DISPLAY ]]; then
+	# Running X11.
+	sudo dnf install -y xclip
+fi
+
+
 # Install rust.
 if [ ! -f ~/.cargo/bin/cargo ]; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
