7d57ec3fcfd0f7109ae7a3b9dab6a5589cd3d2db
Russ Goetz <russgoetz@gmail.com>
2025-08-28 20:18:52 -0400

    Added dev tools for ESP using rust

--- install/development.sh
+++ install/development.sh
@@ -3,6 +3,9 @@
 # Install core development tools.
 sudo dnf install -y @development-tools &> /dev/null
 
+# Install tmux.
+sudo dnf install -y tmux
+
 # Install neovim.
 sudo dnf install -y neovim &> /dev/null
 rm -rfd ~/.config/nvim &> /dev/null || true
@@ -23,5 +26,12 @@ if [ ! -f ~/.cargo/bin/cargo ]; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-# Install tmux.
-sudo dnf install -y tmux
+# Install ESP rust tools.
+# Install perl (required to build OpenSSL for espup).
+sudo dnf install -y perl
+cargo install espup --locked
+espup install
+cargo install esp-generate
+cargo install espflash
+cargo install cargo-espflash
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
