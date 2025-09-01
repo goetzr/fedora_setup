fa7c4776f45d2b1d7941bd32d58cac0ac497fc1f
Russ Goetz <russgoetz@gmail.com>
2025-09-01 16:46:11 -0400

    Set git editor to neovim

--- install/1-git.sh
+++ install/1-git.sh
@@ -4,3 +4,4 @@ git config --global pull.rebase true
 git config --global init.defaultBranch main
 git config --global user.name "Russ Goetz"
 git config --global user.email "russgoetz@gmail.com"
+git config --global core.editor "nvim"
