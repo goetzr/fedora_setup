5aed909550c456a9486946289820ad2dfd0fbfe0
Russ Goetz <russgoetz@gmail.com>
2025-09-21 16:57:39 -0400

    Redirecting several dnf install commands to /dev/null

--- install/development.sh
+++ install/development.sh
@@ -4,7 +4,7 @@
 sudo dnf install -y @development-tools &> /dev/null
 
 # Install tmux.
-sudo dnf install -y tmux
+sudo dnf install -y tmux &> /dev/null
 
 # Install neovim.
 sudo dnf install -y neovim &> /dev/null
@@ -15,11 +15,11 @@ stow -d ~/dotfiles nvim
 # Install xclip to use as the clipboard provider for neovim when running X11.
 if [[ -n $DISPLAY ]]; then
 	# Running X11.
-	sudo dnf install -y xclip
+	sudo dnf install -y xclip &> /dev/null
 fi
 
 # Install the luarocks package manager for lua.
-sudo dnf install -y luarocks
+sudo dnf install -y luarocks &> /dev/null
 
 # Install rust.
 if [ ! -f ~/.cargo/bin/cargo ]; then
@@ -28,16 +28,13 @@ fi
 
 # Install ESP rust tools.
 # Install perl (required to build OpenSSL for espup).
-sudo dnf install -y perl
+sudo dnf install -y perl &> /dev/null
 cargo install espup --locked
 espup install
 cargo install esp-generate
 cargo install espflash
 cargo install cargo-espflash
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
-#
-# Install tmux.
-sudo dnf install -y tmux
 
 # Install the uv package manager for python.
 curl -LsSf https://astral.sh/uv/install.sh | sh
